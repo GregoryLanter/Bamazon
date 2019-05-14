3707ad9499c7604545533d24ae0eff577b4dcdd2
Greg Lanter <dohnut22.gl@gmail.com>
2019-05-14 11:43:32 -0700

    added table departments and updated table products with new column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+//schema for bamazon product table
 Create database if not exists bamazon;
 use bamazon;
 create table if not exists products (
@@ -7,4 +8,18 @@ create table if not exists products (
     price decimal(6,2),
     stock_quantity integer(6)
     );
-    
+
+//schema for bamazon department table
+use bamazon;
+
+create table departments(
+	department_id integer(4) auto_increment primary key,
+    department_name varchar(50),
+    over_head_costs decimal(4,2)
+    );
+
+
+//schema for adding product_sales to product table
+use bamazon;
+alter table products
+add column product_sales integer(9) after stock_quantity;
